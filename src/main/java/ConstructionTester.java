f14e98defe8e6fc7759ec813a5753a58a664760c
Diego Alfaro-Carcoba <diego.225455@nv.ccsd.net>
2020-09-25 17:01:21 +0000

    I worked on Construction

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -7,9 +7,19 @@ public class ConstructionTester
       double taxRate;
       int boards;
       int numWindows;
+
       Scanner myScanner = new Scanner(System.in);
       Construction myConstruction = new Construction();
+
       System.out.print("Enter the sales tax rate: ");
       taxRate = myScanner.nextDouble();
+
+      System.out.println("How many boards do you need?");
+      boards = myScanner.nextInt();
+
+      System.out.println("How many windows do you need?");
+      numWindows = myScanner.nextInt();
+
+      Construction myConstruction = new Construction();
     }
 }
